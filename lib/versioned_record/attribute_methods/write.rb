1be961c55883d072b1bb060973dfd58291544745
Brenton "B-Train" Fletcher <i@bloople.net>
2014-08-13 10:07:49 +1000

    [PR#8] A user might have an attribute that can take an array as it's value, so just checking that value is an array is not specific enough; check specifically for composite keys object

--- lib/versioned_record/attribute_methods/write.rb
+++ lib/versioned_record/attribute_methods/write.rb
@@ -4,9 +4,8 @@ module ActiveRecord
       alias_method :write_attribute_original, :write_attribute
 
       def write_attribute(attr_name, value)
-        #byebug if attr_name == 'contract_id' && Apprenticeship === self
-        # We only have a single value to set, but an array was provided
-        if !attr_name.kind_of?(Array) && value.kind_of?(Array)
+        # We only have a single value to set, but a composite key array was provided
+        if !attr_name.kind_of?(Array) && value.kind_of?(CompositePrimaryKeys::CompositeKeys)
           # Use just the ID and ignore the version
           write_attribute_original(attr_name, value[0])
         else
